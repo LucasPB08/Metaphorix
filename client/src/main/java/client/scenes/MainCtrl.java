0baceb3eafed9f4a4dbb49d394dc2415aed90fa7
Lucas Pereira Barrios <l.pereirabarrios@student.tudelft.nl>
2023-07-11 22:13:17 +0200

    Style textfield & button

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -49,6 +49,11 @@ public class MainCtrl {
 
         showSignIn();
         stage.show();
+        setStyleSheet();
+    }
+
+    private void setStyleSheet(){
+        this.signInScene.getStylesheets().add("stylesheets/controls-styling.css");
     }
 
     /**
